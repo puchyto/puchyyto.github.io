5421063cd149ebb7de69caf79db387b88f2036c0
luchy <luchytochikito@gmail.com>
2024-04-02 19:39:06 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+window.sr = ScrollReveal();
+sr.reveal('#titulo', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('#ul', {
+    duration: 1500,
+    origin: 'left',
+    distance: '-10px'
+});
+sr.reveal('#ul2', {
+    duration: 1500,
+    origin: 'right',
+    distance: '-10px'
+});
+sr.reveal('#ratita', {
+    duration: 7000,
+    rotate: {
+        x: 80,
+        y: 31,
+        z: 20,
+    }
+});
+sr.reveal('#c1', {
+    duration: 5000,
+    rotate: {
+        x: 30,
+        y: 11,
+        z: 10,
+    }
+});
+sr.reveal('#c2', {
+    duration: 5000,
+    rotate: {
+        x: 30,
+        y: 11,
+        z: 10,
+    }
+});
+sr.reveal('#abrazo', {
+    duration: 4000,
+    origin: 'right',
+    distance: '30px',
+});
+sr.reveal('#uwu', {
+    duration: 3000,
+    distance: '10px',
+});
+sr.reveal('#trauma', {
+    duration: 4000,
+    rotate: {
+        x: 40,
+        y: 2,
+    }
+});
+sr.reveal('#bb', {
+    duration: 9070,
+    rotate: {
+        x: 100,
+        y: 2,
+    }
+});
+sr.reveal('#chingue', {
+    duration: 9000,
+    rotate: {
+        x: 100,
+        y: 2,
+        z: 40,
+    }
+});
+sr.reveal('#c3', {
+    duration: 5000,
+    rotate: {
+        x: 30,
+        y: 11,
+        z: 10,
+    }
+});
+sr.reveal('#cartita',{
+    duration: 4000,
+    distance: '-20px',
+})
+sr.reveal('#bb2',{
+    duration: 1050,
+    origin: "bottom",
+    distance:'20px',
+})
+sr.reveal('#c4', {
+    duration: 5000,
+    rotate: {
+        x: 30,
+        y: 11,
+        z: 10,
+    }
+});
+sr.reveal('#bb3',{
+    duration: 4000,
+    origin: "bottom",
+    distance:'20px',
+})
+sr.reveal('#aia',{
+    duration: 1050,
+    distance:'-20px',
+})
